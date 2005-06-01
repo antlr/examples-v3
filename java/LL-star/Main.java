05b9bb87376cc557726cd5c2e126be3e66d959fc
parrt <parrt@antlr.org>
2005-05-31 16:16:09 -0800

    new example
    [git-p4: depot-paths = "//depot/code/examples-v3/": change = 1754]

---
+++ java/LL-star/Main.java
@@ -0,0 +1,12 @@
+import org.antlr.runtime.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		CharStream input = new ANTLRFileStream(args[0]);
+		SimpleCLexer lex = new SimpleCLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lex);
+		System.out.println("tokens="+tokens);
+		SimpleC parser = new SimpleC(tokens);
+		parser.program();
+	}
+}
