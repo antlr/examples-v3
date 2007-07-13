82a9e3a183faddce9ab0b59761351ae0453de138
parrt <parrt@antlr.org>
2007-07-13 13:50:16 -0800

    add v3 grammar
    [git-p4: depot-paths = "//depot/code/examples-v3/": change = 3899]

---
+++ java/ANTLR/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+	CharStream input = null;
+	if ( args.length==1 ) {
+	    input = new ANTLRFileStream(args[0]);
+	}
+	else {
+	    input = new ANTLRInputStream(System.in);
+	}
+        ANTLRv3Lexer lex = new ANTLRv3Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lex);
+        ANTLRv3Parser g = new ANTLRv3Parser(tokens);
+        ANTLRv3Parser.grammarDef_return r = g.grammarDef();
+	CommonTree t = (CommonTree)r.getTree();
+	System.out.println(t.toStringTree());
+    }
+}
