5dc80846533a1f633aae9e008ed8e587ba792fe5
parrt <parrt@antlr.org>
2007-12-27 16:46:24 -0800

    Fix ANTLR-165.
    [git-p4: depot-paths = "//depot/code/examples-v3/": change = 4394]

--- java/fuzzy/Main.java
+++ java/fuzzy/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 	    for (int i=0; i<args.length; i++) {
 		CharStream input = new ANTLRFileStream(args[i]);
-		FuzzyJavaLexer lex = new FuzzyJavaLexer(input);
+		FuzzyJava lex = new FuzzyJava(input);
 		TokenStream tokens = new CommonTokenStream(lex);
 		tokens.toString();
 		//System.out.println(tokens);
