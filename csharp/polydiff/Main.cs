bdba97afd3a0b13ba9afc88d3cf44b02b18682c5
Johannes Luber <jaluber@gmx.de>
2008-05-06 11:31:55 -0800

    [C#] - Synced up to 4855 - Fixed bugs for debug generation - Fixed wrong capitalization and example
    [git-p4: depot-paths = "//depot/code/examples-v3/": change = 4856]

--- csharp/polydiff/Main.cs
+++ csharp/polydiff/Main.cs
@@ -42,6 +42,6 @@ namespace Antlr.Examples.PolyDiff
         nodes.TokenStream = tokens;
         PolyPrinter printer = new PolyPrinter(nodes);
 		PolyPrinter.poly_return r4 = printer.poly();
-		Console.Out.WriteLine(r4.St);
+		Console.Out.WriteLine(r4.ST);
 	}	}
 }
