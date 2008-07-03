e0089c74b64a3f08802486f2d6f59175770d293c
parrt <parrt@antlr.org>
2008-07-03 10:40:09 -0800

    Fix ANTLR-267, parse trees were hosed during backtracking and cyclic DFA; tracks hidden tokens too now. Added toInputString() to  get text back including hidden tokens.  Shows <epsilon> for rules  that match nothing.
    [git-p4: depot-paths = "//depot/code/examples-v3/": change = 5027]

--- java/ANTLR/Main.java
+++ java/ANTLR/Main.java
@@ -1,25 +1,48 @@
 import java.io.*;
 import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
+import org.antlr.runtime.debug.*;
 
+/** Build either ASTs and, optionally, parse trees.  To also build
+ *  parse trees, run antlr with -debug option on ANTLRv3.g
+ *  and then uncomment the middle code section and comment out the
+ *  first "build AST" only section.
+ */
 public class Main {
     public static void main(String args[]) throws Exception {
-	CharStream input = null;
-	if ( args.length==1 ) {
-	    input = new ANTLRFileStream(args[0]);
-	}
-	else {
-	    input = new ANTLRInputStream(System.in);
-	}
+        CharStream input = null;
+        String inputName = "<stdin>";
+        if ( args.length==1 ) {
+            input = new ANTLRFileStream(args[0]);
+            inputName = args[0];
+        }
+        else {
+            input = new ANTLRInputStream(System.in);
+        }
+
+        // BUILD AST
         ANTLRv3Lexer lex = new ANTLRv3Lexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lex);
         ANTLRv3Parser g = new ANTLRv3Parser(tokens);
         ANTLRv3Parser.grammarDef_return r = g.grammarDef();
-	CommonTree t = (CommonTree)r.getTree();
-	System.out.println(t.toStringTree());
+        CommonTree t = (CommonTree)r.getTree();
+        System.out.println(t.toStringTree());
 
-	CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
-	ANTLRv3Tree walker = new ANTLRv3Tree(nodes);
-	walker.grammarDef();
+/*
+        // BUILD AST + PARSE TREES
+        ANTLRv3Lexer lex = new ANTLRv3Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lex);
+        ParseTreeBuilder builder = new ParseTreeBuilder(inputName);
+        ANTLRv3Parser g = new ANTLRv3Parser(tokens, builder);
+        ANTLRv3Parser.grammarDef_return r = g.grammarDef();
+        CommonTree t = (CommonTree)r.getTree();
+        System.out.println("parse tree: "+builder.getTree().toStringTree());
+        System.out.print("input:\n"+builder.getTree().toInputString());
+*/
+
+        // WALK AST
+        CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
+        ANTLRv3Tree walker = new ANTLRv3Tree(nodes);
+        walker.grammarDef();
     }
 }
